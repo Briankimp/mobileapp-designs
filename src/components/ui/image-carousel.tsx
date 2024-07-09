b4e4c0d9db2e0f8a72bfb7e56f48febb5d4aa26e
HillaryMurimi <hillarymurimi328@gmail.com>
2024-07-09 14:47:55 +0300

    Fix: Resolved issue with image carousel

--- src/components/ui/image-carousel.tsx
+++ src/components/ui/image-carousel.tsx
@@ -12,7 +12,7 @@ const App: React.FC<ImageCarouselProps> = ({ imageUrls }) => (
   <Carousel effect="fade">
     {imageUrls.map((url, index)=>(
       <div key={index} className='h-[200px] w-366px bg-gray-800 flex items-center justify-center'>
-        <Image src={url} alt={`Slide ${index + 1}`} className='h-full w-full object-cover' />
+        <img src={url} alt={`Slide ${index + 1}`} className='h-full w-full object-cover' />
       </div>
     ))}
   </Carousel>
