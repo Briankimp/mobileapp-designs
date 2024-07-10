162435c325807f9e96799bdeaad2c98c1b78ca28
HillaryMurimi <hillarymurimi328@gmail.com>
2024-07-10 16:58:43 +0300

    listings/ads

--- src/app/nx/page.tsx
+++ src/app/nx/page.tsx
@@ -1,12 +1,41 @@
 import React from "react";
+import { CircleUserRound, Search, Logs } from 'lucide-react';
+import HorizontalScrollButtons from "@/components/ui/HorizontalScrollButtons";
+import ListingCard from "@/components/ui/ListingCard";
+
 
 const HomePage = () => {
   return (
     <>
-      <div className="px-8">
-        <div className="bg-blue-100 shadow rounded-[30px] ">
-          <h1>This is the homepage. It will contain listings/ads</h1>
+      <div className="flex flex-col pt-11 px-8 w-full bg-white-100">
+
+        <div className="">
+          <CircleUserRound />
         </div>
+
+        <div className="my-11 flex flex-row justify-between">
+          <Search color="black"/>
+          <Logs color="black"/>
+        </div>
+
+        <div>
+          <HorizontalScrollButtons/>
+        </div>
+
+        <div className="my-4 flex flex-col bg-white-100">
+          <ListingCard/>
+          <ListingCard/>
+          <ListingCard/>
+          <ListingCard/>
+          <ListingCard/>
+          <ListingCard/>
+          <ListingCard/>
+          <ListingCard/>
+          <ListingCard/>
+          <ListingCard/>
+          <ListingCard/>
+        </div>
+
       </div>
     </>
   );
