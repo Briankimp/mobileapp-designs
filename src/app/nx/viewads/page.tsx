b3c88defbfc0fe564cc1ec285d1fdd623bfc96dc
Brian <bkimathi973@gmail.com>
2024-07-11 17:59:03 +0300

    View All Ads Code

---
+++ src/app/nx/viewads/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewAd = () => {
+  return (
+    <div>
+      <h1> Brian </h1>
+    </div>
+  )
+}
+
+export default ViewAd
---
+++ src/components/ui/viewad.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+
+function ViewAd() {
+  return (
+    <div>
+        div
+      <Image
+      src='centralized\public\viewads.png'
+      alt='Ad Image'
+      width={50}
+      height={50}
+      className = 'rounded'
+      />
+    </div>
+  )
+}
+
+export default ViewAd
